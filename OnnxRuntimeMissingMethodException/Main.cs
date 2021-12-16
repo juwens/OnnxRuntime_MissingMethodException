682f2afdb6cd83f5d7dac7c2fd61b43d8e4e95af
Jens-Uwe Jährig <jens-uwe.jaehrig@fsd-web.de>
2021-12-16 16:59:38 +0100

    added class library

--- OnnxRuntimeMissingMethodException/Main.cs
+++ OnnxRuntimeMissingMethodException/Main.cs
@@ -1,4 +1,7 @@
-﻿using UIKit;
+﻿using System;
+using LibraryUsingOnnxRuntime;
+using ObjCRuntime;
+using UIKit;
 
 namespace OnnxRuntimeMissingMethodException
 {
@@ -7,6 +10,16 @@ namespace OnnxRuntimeMissingMethodException
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            var a = new System.Memory<float>();
+            var b = new Microsoft.ML.OnnxRuntime.Tensors.DenseTensor<float>(1);
+            var c = new System.Memory<Microsoft.ML.OnnxRuntime.Tensors.DenseTensor<float>>();
+            var d = b.Buffer;
+
+            Console.WriteLine(typeof(System.Memory<>).AssemblyQualifiedName);
+
+            Class1.PrintMemoryVersion();
+            Class1.TestDenseTensor();
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, typeof(AppDelegate));
